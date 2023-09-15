c41d8626dac80d130552223ed12c9b67ffdcd055
mynedear <73683453+mynedear@users.noreply.github.com>
2023-09-15 17:56:17 +0700

    Update QueryData.py
    sort data

--- QueryData.py
+++ QueryData.py
@@ -8,8 +8,8 @@ data = {
     "query": {
         "bool": {
             "filter":[
-                {"terms": {"intent": ["ELEMENT_ACTIVATED","ELEMENT_COMPLETED"]}},
-                {"match": {"value.processInstanceKey": 2251799813721617}}  
+                {"terms": {"intent": ["ELEMENT_ACTIVATED","ELEMENT_COMPLETED","ELEMENT_TERMINATED"]}},
+                {"match": {"value.processInstanceKey": 	2251799813694049}}  
             ]
         }
     }
@@ -24,7 +24,7 @@ response_json = response.json()
 elementId_List = []
 intent_List = []
 bpmnElementType_List = []
-timestamp_List = []
+sequence_List = []
 element_dict = {}
 
 for body in response_json['hits']['hits']:
@@ -32,20 +32,47 @@ for body in response_json['hits']['hits']:
     elementId = body['_source']['value']['elementId']
     intent = body['_source']['intent']
     bpmnElementType = body['_source']['value']['bpmnElementType']
-    timestamp = body['_source']['timestamp']
+    sequence = body['_source']['sequence']
 
     if intent == 'ELEMENT_ACTIVATED':
         element_dict[elementId] = {
             'elementId': elementId,
             'intent': intent,
             'bpmnElementType': bpmnElementType,
-            'timestamp': timestamp
+            'sequence': sequence
         }
-    elif intent == 'ELEMENT_COMPLETED' and elementId in element_dict:
-        element_dict[elementId]['intent'] = intent
-        element_dict[elementId]['bpmnElementType'] = bpmnElementType
-        element_dict[elementId]['timestamp'] = timestamp
+    elif intent == 'ELEMENT_COMPLETED':
+
+        element_dict[elementId] = {
+            'elementId': elementId,
+            'intent': intent,
+            'bpmnElementType': bpmnElementType,
+            'sequence': sequence
+        }
+
     
 result_list = list(element_dict.values())
 df = pd.DataFrame(result_list)
-print(df)
+# print(df)
+# print(result_list)
+df_sorted = df.sort_values(by='sequence', ascending=True)
+print(df_sorted)
+
+# df_sorted.loc[df_sorted['elementId'] == 'download', 'intent'] = 'FAIL'
+# print(df_sorted)
+
+data2 = {
+    'elementId': ['download'],
+    'intent': ['CREATED'],
+    'bpmnElementType': ['bpmnchromedriver'],
+    'sequence': [2251799813685251]
+}
+
+df2 = pd.DataFrame(data2)
+print(df2)
+
+df_sorted.loc[df_sorted['elementId'].isin(df2['elementId']), 'intent'] = 'Fail'
+print(df_sorted)
+
+
+
