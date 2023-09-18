07a55d19b0b7712c9e9bb64f4dc627f6d99867f3
mynedear <73683453+mynedear@users.noreply.github.com>
2023-09-18 11:46:09 +0700

    Update QueryData.py
    สร้างฟังก์ชั่น ทำดึงข้อมูล Instance History จากProcess instance Key ทั้งของ Activated,Completed,Termineted,Fail เสร็จแล้วแต่ยังอยู่ในรูปแบบของ dataframe รอปรับโค้ดโดยที่ไม่ต้องใช้dataframe

--- QueryData.py
+++ QueryData.py
@@ -1,78 +1,111 @@
 import pandas as pd
 import requests
-# import json
+def Instance_History(Process_Instance_Key):
 
-url = "http://localhost:9200/zeebe-record_process-instance*/_search"
-data = {
-    "size": 10000,
-    "query": {
-        "bool": {
-            "filter":[
-                {"terms": {"intent": ["ELEMENT_ACTIVATED","ELEMENT_COMPLETED","ELEMENT_TERMINATED"]}},
-                {"match": {"value.processInstanceKey": 	2251799813694049}}  
-            ]
+    url = "http://localhost:9200/zeebe-record_process-instance*/_search"
+    data = {
+        "size": 10000,
+        "sort": [
+                    {
+                    "sequence" : {
+                        "order": "asc"
+                    }
+                    }
+                ],
+        "query": {
+            "bool": {
+                "filter":[
+                    {"terms": {"intent": ["ELEMENT_ACTIVATED","ELEMENT_COMPLETED","ELEMENT_TERMINATED"]}},
+                    {"match": {"value.processInstanceKey": 	Process_Instance_Key}}  
+                ]
+            }
         }
     }
-}
-
-response = requests.post(url, json=data)
-response_json = response.json()
-# print(response_json)
-# formatted_json = json.dumps(response_json, indent=4)
-# print(formatted_json)
-
-elementId_List = []
-intent_List = []
-bpmnElementType_List = []
-sequence_List = []
-element_dict = {}
-
-for body in response_json['hits']['hits']:
-    
-    elementId = body['_source']['value']['elementId']
-    intent = body['_source']['intent']
-    bpmnElementType = body['_source']['value']['bpmnElementType']
-    sequence = body['_source']['sequence']
-
-    if intent == 'ELEMENT_ACTIVATED':
-        element_dict[elementId] = {
-            'elementId': elementId,
-            'intent': intent,
-            'bpmnElementType': bpmnElementType,
-            'sequence': sequence
+    url_incident = "http://localhost:9200/zeebe-record_incident_8.2.5*/_search"
+    data_incident = {
+        "size": 10000,
+        "query": {
+            "bool": {
+                "filter":[
+                    {"match": {"value.processInstanceKey": 	Process_Instance_Key}}  
+                ]
+            }
         }
-    elif intent == 'ELEMENT_COMPLETED':
+    }
 
-        element_dict[elementId] = {
-            'elementId': elementId,
-            'intent': intent,
-            'bpmnElementType': bpmnElementType,
-            'sequence': sequence
-        }
+    response = requests.post(url, json=data)
+    response_json = response.json()
+    response_incident = requests.post(url_incident, json=data_incident)
+    response_incident_json = response_incident.json()
 
-    
-result_list = list(element_dict.values())
-df = pd.DataFrame(result_list)
-# print(df)
-# print(result_list)
-df_sorted = df.sort_values(by='sequence', ascending=True)
-print(df_sorted)
+    elementId_List = []
+    intent_List = []
+    bpmnElementType_List = []
+    sequence_List = []
+    element_dict = {}
+    element_incident_dict = {}
 
-# df_sorted.loc[df_sorted['elementId'] == 'download', 'intent'] = 'FAIL'
-# print(df_sorted)
+    for body in response_json['hits']['hits']:
+        
+        elementId = body['_source']['value']['elementId']
+        intent = body['_source']['intent']
+        bpmnElementType = body['_source']['value']['bpmnElementType']
+        sequence = body['_source']['sequence']
 
-data2 = {
-    'elementId': ['download'],
-    'intent': ['CREATED'],
-    'bpmnElementType': ['bpmnchromedriver'],
-    'sequence': [2251799813685251]
-}
+        if intent == 'ELEMENT_ACTIVATED':
+            element_dict[elementId] = {
+                'elementId': elementId,
+                'intent': intent,
+                'bpmnElementType': bpmnElementType,
+                'sequence': sequence
+            }
+        elif intent == 'ELEMENT_COMPLETED':
 
-df2 = pd.DataFrame(data2)
-print(df2)
+            element_dict[elementId] = {
+                'elementId': elementId,
+                'intent': intent,
+                'bpmnElementType': bpmnElementType,
+                'sequence': sequence
+            }
+        elif intent == 'ELEMENT_TERMINATED':
 
-df_sorted.loc[df_sorted['elementId'].isin(df2['elementId']), 'intent'] = 'Fail'
-print(df_sorted)
+            element_dict[elementId] = {
+                'elementId': elementId,
+                'intent': intent,
+                'bpmnElementType': bpmnElementType,
+                'sequence': sequence
+            }
+        
+        
+    result_list = list(element_dict.values())
+    df = pd.DataFrame(result_list)
+
+    for body_incident in response_incident_json['hits']['hits']:
+        
+        elementId = body_incident['_source']['value']['elementId']
+        intent = body_incident['_source']['intent']
+        bpmnProcessId = body_incident['_source']['value']['bpmnProcessId']
+        sequence = body_incident['_source']['sequence']
+
+        element_incident_dict[elementId] = {
+                'elementId': elementId,
+                'intent': intent,
+                'bpmnElementType': bpmnProcessId,
+                'sequence': sequence
+            }
+    result_incident_list = list(element_incident_dict.values())
+    df_incident = pd.DataFrame(result_incident_list)
 
 
+    if 'elementId' in df_incident.columns:
+        print("The 'elementId' column exists in df_incident.")
+        df.loc[df['elementId'].isin(df_incident['elementId']), 'intent'] = 'Fail'
+        
+    else:
+        print("The 'elementId' column does not exist in df_incident.")
 
+    return df
+
+
+x = Instance_History(Process_Instance_Key=2251799813698800) 
+print(x)
